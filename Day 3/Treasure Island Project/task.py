4d167b39dbbda2fe5cb61eed5d073a3333dc49cf
Code-Odyssey <andrewsmmm@outlook.com>
2026-05-28 15:00:26 +0100

    Trying with a new clean commit

--- Day 3/Treasure Island Project/task.py
+++ Day 3/Treasure Island Project/task.py
@@ -1,3 +1,4 @@
+'''this is my solution to the Treasure Island Project'''
 print(r'''
 *******************************************************************************
           |                   |                  |                     |
