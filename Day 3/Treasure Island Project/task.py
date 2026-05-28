5ba65a4cfe09908e1c6a8ec58600ccb9938526b8
Mark Andrews <andrewsmmm@outlook.com>
2026-05-28 09:38:22 +0100

    Hopefully complete the Treasure Map game challenge

--- Day 3/Treasure Island Project/task.py
+++ Day 3/Treasure Island Project/task.py
@@ -26,40 +26,44 @@ print(f"Welcome to Treasure Island, {name}!")
 print("Your mission is to find the treasure.")
 choice_1 = input("You come to a fork in the road. "
                  "Do your want to go (type) left or right? ").lower()
-
 if choice_1 == "left":
     choice_2 = input("You have come to a fast flowing river. "
                      "Do you wade across or wait for the ferryman? "
                      "Enter ferryman or wade: ").lower()
+
     if choice_2 == "ferryman":
         print("Sadly, the ferryman is death. GAME OVER!")
     elif choice_2 == "wade":
         print("You get washed downstream but end up on the other side of the river!")
         print("You walk on a little further and come to an old cottage with three "
-              "coloured doors. One red, one yellow and one blue. "
+              "coloured doors. One red, one yellow and one white. "
               "Which door do you choose? ")
-        choice_3 = input("Enter red, yellow or white: ")
+        choice_3 = input("Enter red, yellow or white: ").lower()
         if choice_3 == "red":
             print("You are engulfed in fire. GAME OVER!")
         elif choice_3 == "yellow":
             print("You have woken the dragon and are eaten. GAME OVER!")
         elif choice_3 == "white":
             print("You have found the treasure! YOU WIN!")
+        else:
+            print("Invalid choice. GAME OVER!")
 
 
 elif choice_1 == "right":
     choice_4 = input("You have come to a deep ravine. You can either climb down the rope or use the rickety bridge. "
-                     "Enter climb or bridge: ")
+                     "Enter climb or bridge: ").lower()
     if choice_4 == "climb":
         print("The rope snapped and you fall to your death. GAME OVER!")
     elif choice_4 == "bridge":
         print("You made it across the rickety bridge but only just. ")
         print("You have reached an old cottage with three coloured doors")
-        choice_5 = input("One red, one yellow and one blue. Which door do you choose? "
-                         "Enter red, yellow or white: ")
+        choice_5 = input("One red, one yellow and one white. Which door do you choose? "
+                         "Enter red, yellow or white: ").lower()
         if choice_5 == "red":
             print("You are engulfed in fire. GAME OVER!")
         elif choice_5 == "yellow":
             print("You have woken the dragon and are eaten. GAME OVER!")
         elif choice_5 == "white":
             print("You have found the treasure! YOU WIN!")
+        else:
+            print("Invalid choice. GAME OVER!")
