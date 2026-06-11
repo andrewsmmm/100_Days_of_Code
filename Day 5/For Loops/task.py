4c4d21f9c6e046e7d42e6f6fca080833d565d0c2
Code-Odyssey <andrewsmmm@outlook.com>
2026-06-11 14:15:14 +0100

    Current work on 100 Days - just did for loops and the Highest Score Challenge

--- Day 5/For Loops/task.py
+++ Day 5/For Loops/task.py
@@ -1,1 +1,15 @@
 fruits = ["Apple", "Peach", "Pear"]
+for fruit in fruits:
+    print(fruit)
+    print(fruit + " pie")
+
+fruits = ["Apple", "Peach", "Pear"]
+for fruit in fruits:
+    print(fruit)
+    print("Hello\n")
+
+fruits = ["Apple", "Peach", "Pear"]
+for fruit in fruits:
+    print(fruit)
+print("Hello")
+
--- Day 5/Highest Score/task.py
+++ Day 5/Highest Score/task.py
@@ -1,2 +1,10 @@
-student_scores = [150, 142, 185, 120, 171, 184, 149, 24, 59, 68, 199, 78, 65, 89, 86, 55, 91, 64, 89]
-print(range(1, 10))
+# student_scores = [150, 142, 185, 120, 171, 184, 149, 24, 59, 68, 199, 78, 65, 89, 86, 55, 91, 64, 89]
+# print(range(1, 10))
+
+# Here is my solution to the problem:
+student_scores2 = [8, 65, 89, 86, 55, 91, 64, 89]
+max_score = 0
+for num in student_scores2:
+    if num > max_score:
+        max_score = num
+print(max_score)
