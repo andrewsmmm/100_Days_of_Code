a9f8185b51e301bffd36edc3e6c5ec7efe4018a0
Code-Odyssey <andrewsmmm@outlook.com>
2026-06-18 08:18:25 +0100

    Minor updates to files--clean up.

--- Day 1/task/task.py
+++ Day 1/task/task.py
@@ -1,5 +1,7 @@
+# Band Name Generator
+
 name = input("Enter your name: \n ")
-print ("Hello, " + name + ", welcome to the Band Name Generator")
+print("Hello, " + name + ", welcome to the Band Name Generator")
 city = input("Enter the city where you grew up:\n ")
 pet_name = input("Enter your pet's name: \n ")
-print (f"{name}, your ideal band name could be {city} {pet_name}")
+print(f"{name}, your ideal band name could be {city} {pet_name}")
